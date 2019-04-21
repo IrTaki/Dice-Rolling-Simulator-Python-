2544815af5c9543c03caa0be6921b639a88dc1d8
Nick Cattley <16906343+NickCattley@users.noreply.github.com>
2019-04-21 11:45:50 +0100

    Code commented

--- dice.py
+++ dice.py
@@ -2,6 +2,8 @@ import os
 import random
 
 
+# Dice rolling function to keep main code cleaner.
+# Quantity always equals 1 in case the user doesn't enter any number.
 def roll_dice(quantity=1):
     os.system("cls" if os.name == "nt" else "clear")
     x = 1
@@ -14,23 +16,22 @@ def roll_dice(quantity=1):
         print("Your die rolled: ", random.randint(1, 6))
 
 
-while True:
+while True:  # Infinite loop to keep rolling dice until the user quits.
     print("Welcome to the dice rolling app!\n"
           "--------------------------------\n"
           "Press 1 to roll some dice.\n"
           "Press 2 to quit.\n")
-
-    user_choice = input()
+    user_choice = input()  # Get users choice for the menu options.
     os.system("cls" if os.name == "nt" else "clear")
 
-    if int(user_choice) == 1:
+    if user_choice == "1":  # Roll some dice.
         number_of_dice = input("How many dice do you want to roll?\n")
         roll_dice(int(number_of_dice))
         input("\nPress any key to continue")
         os.system("cls" if os.name == "nt" else "clear")
-    elif int(user_choice) == 2:
+    elif user_choice == "2":  # Quits the application.
         break
-    else:
+    else:  # Repeats the question if they enter an invalid response.
         print("Invalid option.")
         input("\nPress any key to continue")
         os.system("cls" if os.name == "nt" else "clear")
